c82e43b1492fdbab8ec75f07a148b6eb73317c84
Tadhg Creedon <tadhgcreedon@gmail.com>
2016-09-07 11:20:01 -0400

    add react + webpack boilerplate, add node_modules to .gitignore

---
+++ app/app.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+
+class Test extends Component {
+    render() {
+        return(
+            <div>test</div>
+        );
+    }
+}
+
+ReactDOM.render(<Test/>, document.getElementById('render-target'));
---
+++ webpack.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+    entry: "./app/app.jsx",
+    output: {
+        path: __dirname,
+        filename: "bundle.js"
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.jsx?$/,
+                exclude: /node_modules/,
+                loader: "babel-loader",
+                query: {
+                    presets: ['es2015', 'react']
+                }
+            }
+        ]
+    }
+};
