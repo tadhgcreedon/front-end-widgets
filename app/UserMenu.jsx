d75d49b5a4a6f9868d2af9f7df81c7d3efe2c682
Tadhg Creedon <tadhgcreedon@gmail.com>
2016-09-07 12:46:45 -0400

    add button badge functionality to widget contents JSON file and UserMenu.jsx

--- app/UserMenu.jsx
+++ app/UserMenu.jsx
@@ -3,12 +3,24 @@ import React, { Component } from 'react';
 export default class UserMenu extends Component {
     render(){
         let buttons = this.props.content['buttons'].map((button) => {
-            return <button className='user_menu_button' key={button.order}>{button.label}</button>;
+            return(
+                <button className='user_menu_button' key={button.order}>
+                  {button.image}
+                  {button.label}
+                  {/*button.notifications > 0 ? <Badge notification_number={button.notifications}/> : null */}
+                </button>
+            );
         });
         return(
-            <section id='user_menu_container'>
-              {buttons}
-            </section>
+            <section id='user_menu_container'>{buttons}</section>
+        );
+    }
+}
+
+class Badge extends Component {
+    render() {
+        return(
+            <section className='user_menu_button_badge'>{this.props.notification_number}</section>
         );
     }
 }
