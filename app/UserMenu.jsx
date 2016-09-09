45ddae7655c233f120a0137d1ce911fa69681e7a
Tadhg Creedon <tadhgcreedon@gmail.com>
2016-09-09 10:14:51 -0400

    add user menu click functionality

--- app/UserMenu.jsx
+++ app/UserMenu.jsx
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 export default class UserMenu extends Component {
     constructor(props) {
         super(props);
-  
+
         let selected = {};
         this.buttons = [];
         this.props.content['buttons'].forEach((button) => {
@@ -30,13 +30,35 @@ export default class UserMenu extends Component {
         }
     }
 
+    buttonSelectedCustomClass(button_label) {
+        switch(button_label) {
+            case 'CHECK IN':
+                return 'user_menu_button_selected_check_in';
+            case 'EVENTS':
+                return 'user_menu_button_selected_events';
+            case 'ACCOUNT':
+                return 'user_menu_button_selected_account';
+            case 'SETTINGS':
+                return 'user_menu_button_selected_settings';
+            default:
+                return '';
+        }
+    }
+
     render(){
+        let buttons_selected_classes;
         let buttons = this.props.content['buttons'].map((button, index) => {
+            if(this.state.selected[button.label] === true) {
+                buttons_selected_classes = ' user_menu_button_selected  ' + this.buttonSelectedCustomClass(button.label);
+            }
+            else {
+                buttons_selected_classes = '';
+            }
             return(
-                <button className={'user_menu_button'} key={index} onClick={() => this.handleButtonClick(button.label)}>
+                <button className={'user_menu_button' + buttons_selected_classes} key={index} onClick={() => this.handleButtonClick(button.label)}>
                   <img className='user_menu_button_icon' src={button.icon_path} />&nbsp;
                   <span className='user_menu_button_text'>
-                    {button.label}
+                    <span className="user_menu_button_text_label">{button.label}</span>
                     {button.notifications > 0 ? <Badge notification_number={button.notifications}/> : null}
                   </span>
                 </button>
