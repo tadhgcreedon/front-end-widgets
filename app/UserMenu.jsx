f31f9801560cf970614cc816aba17afb8a2ab7a0
Tadhg Creedon <tadhgcreedon@gmail.com>
2016-09-09 08:36:57 -0400

    add user menu button click state change

--- app/UserMenu.jsx
+++ app/UserMenu.jsx
@@ -1,10 +1,39 @@
 import React, { Component } from 'react';
 
 export default class UserMenu extends Component {
+    constructor(props) {
+        super(props);
+  
+        let selected = {};
+        this.buttons = [];
+        this.props.content['buttons'].forEach((button) => {
+            this.buttons.push(button.label);
+            selected[button.label] = false;
+        });
+        this.state = { selected };
+    }
+
+    handleButtonClick(button_label){
+        for(let button in this.state.selected) {
+            let newSelected = this.state.selected;
+            if(this.buttons.indexOf(button) !== -1 && button === button_label) {
+                newSelected[button] === true ? newSelected[button] = false : newSelected[button] = true;
+            }
+            else {
+                if(this.buttons.indexOf(button) !== -1) {
+                    newSelected[button] = false;
+                }
+            }
+            this.setState({selected: newSelected}, () => {
+                console.log(newSelected);
+            });
+        }
+    }
+
     render(){
         let buttons = this.props.content['buttons'].map((button, index) => {
             return(
-                <button className='user_menu_button' key={index}>
+                <button className={'user_menu_button'} key={index} onClick={() => this.handleButtonClick(button.label)}>
                   <img className='user_menu_button_icon' src={button.icon_path} />&nbsp;
                   <span className='user_menu_button_text'>
                     {button.label}
